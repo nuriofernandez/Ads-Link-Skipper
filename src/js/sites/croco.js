79dd3a3cdfae332ac7f03c39deb50497ef57fc46
PushpenderSaini0 <54404738+PushpenderSaini0@users.noreply.github.com>
2021-06-21 10:25:47 +0530

    (#24) Changed functions and variable declarations to match ES6 standard at croco.js

--- src/js/sites/croco.js
+++ src/js/sites/croco.js
@@ -9,20 +9,20 @@ Script for croco.me
 setTimeout(skip, 100);
 
 function skip(){
-    var time = setTimeout(skip, 100);
-    var ads = xp('//*[@id="form-captcha"]');
+    const time = setTimeout(skip, 100);
+    const ads = xp('//*[@id="form-captcha"]');
     if(ads != null){
         clearTimeout(time);
         ads.submit();
     }
-    var ads = xp('//*[@id="btn-main"]');
+    const ads = xp('//*[@id="btn-main"]');
     if(ads != null){
         clearTimeout(time);
         window.location.replace(ads.href);
     }
     if(document.body != null && xp('//*[@id="Notify"]') == null){
-        var html = '<div id="Notify" style="position: fixed;bottom: 10px;color: #fff;background-color: #ff6060;width: 100%;z-index: 999999;text-align: center;font-size: 23px;padding: 21px;">Ads link skiper is trying to bypass this site... If you found any error please report it to personal@xxnurioxx.me <a href="javascript:document.getElementById(\'Notify\').remove();" style="color: #fff;font-weight: 800;background-color: #d95b57;border-radius: 15px;padding: 8px;margin-left: 20px;">Hide this</a></div>';
-        var div = document.createElement('div');
+        const html = '<div id="Notify" style="position: fixed;bottom: 10px;color: #fff;background-color: #ff6060;width: 100%;z-index: 999999;text-align: center;font-size: 23px;padding: 21px;">Ads link skiper is trying to bypass this site... If you found any error please report it to personal@xxnurioxx.me <a href="javascript:document.getElementById(\'Notify\').remove();" style="color: #fff;font-weight: 800;background-color: #d95b57;border-radius: 15px;padding: 8px;margin-left: 20px;">Hide this</a></div>';
+        const div = document.createElement('div');
         div.innerHTML = html;
         document.body.appendChild(div);
     }
