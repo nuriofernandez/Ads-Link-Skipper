fa0fc5e1bd00fe1f1ea3525f2b3532ec6ac06c50
PushpenderSaini0 <54404738+PushpenderSaini0@users.noreply.github.com>
2021-06-21 10:17:55 +0530

    (#24) Changed functions and variable declarations to match ES6 standard at adfly.js

--- src/js/sites/adfly.js
+++ src/js/sites/adfly.js
@@ -10,29 +10,23 @@ Script for adf.ly
 // Thankyou to https://github.com/StoreClerk/AdF.ly-Skipper/blob/master/AdF.ly%20Skipper/js/script.js
 // Thank you to tote.ervin (PD: Add me to your code too!)
 
-// Event runners
-document.addEventListener('DOMNodeRemoved', skip, false);
-document.addEventListener('DOMContentLoaded', function() {
-    document.removeEventListener('DOMNodeRemoved', skip, false);
-});
-
 /** Skip adf.ly function */
-function skip() {
-    var ysmm = /ysmm = \'(.*?)\';/gi;
-    var code = ysmm.exec(document.getElementsByTagName('html')[0].innerHTML);
+const skip = () => {
+    const ysmm = /ysmm = \'(.*?)\';/gi;
+    let code = ysmm.exec(document.getElementsByTagName('html')[0].innerHTML);
     if (code == null) return;
     code = code[1];
-    var codeLeft = '',
+    let codeLeft = '',
         codeRight = '';
-    for (var i = 0; i < code.length; i++) {
+    for (let i = 0; i < code.length; i++) {
         if (i % 2 == 0) codeLeft += code.charAt(i);
         else codeRight = code.charAt(i) + codeRight;
     }
 
     code = (codeLeft + codeRight).split('');
-    for (var i = 0; i < code.length; i++) {
+    for (let i = 0; i < code.length; i++) {
         if (isNaN(code[i])) continue;
-        for (var j = i + 1; j < code.length; j++) {
+        for (let j = i + 1; j < code.length; j++) {
             if (isNaN(code[j])) continue;
             if ((code[i] ^ code[j]) < 10) code[i] = code[i] ^ code[j];
             i = j;
@@ -46,30 +40,35 @@ function skip() {
     window.location = url;
     // I rlly don't know what is this. By the way i let it here by now xD
     if (document instanceof HTMLDocument) {
-        var script = document.createElement('script');
+        const script = document.createElement('script');
         script.textContent = code;
         document.documentElement.appendChild(script);
     }
 }
 
-/** Adblock Warning banner */
-setTimeout(verifyLock, 500);
+// Event runners
+document.addEventListener('DOMNodeRemoved', skip, false);
+document.addEventListener('DOMContentLoaded', () => {
+    document.removeEventListener('DOMNodeRemoved', skip, false);
+});
 
-function verifyLock() {
-    var time = setTimeout(verifyLock, 500);
-    var elementclick = xp('//*[@id="continue"]/div/a');
+/** xPath function */
+const xp = (query) => {
+    return document.evaluate(query, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+}
+
+const verifyLock = () => {
+    const time = setTimeout(verifyLock, 500);
+    const elementclick = xp('//*[@id="continue"]/div/a');
     if (!(elementclick != null && elementclick.href.length > 0)) return;
 
-    var html = '<div id="Notify" style="position: fixed;bottom: 10px;color: #fff;background-color: #ff6060;width: 100%;z-index: 999999;text-align: center;font-size: 23px;padding: 21px;">Ads link skiper is trying to bypass this site... If you found any error please report it to personal@xxnurioxx.me <a href="javascript:document.getElementById(\'Notify\').remove();" style="color: #fff;font-weight: 800;background-color: #d95b57;border-radius: 15px;padding: 8px;margin-left: 20px;">Hide this</a></div>';
-    var div = document.createElement('div');
+    const html = '<div id="Notify" style="position: fixed;bottom: 10px;color: #fff;background-color: #ff6060;width: 100%;z-index: 999999;text-align: center;font-size: 23px;padding: 21px;">Ads link skiper is trying to bypass this site... If you found any error please report it to personal@xxnurioxx.me <a href="javascript:document.getElementById(\'Notify\').remove();" style="color: #fff;font-weight: 800;background-color: #d95b57;border-radius: 15px;padding: 8px;margin-left: 20px;">Hide this</a></div>';
+    const div = document.createElement('div');
     div.innerHTML = html;
     document.body.appendChild(div);
 
     window.location.replace(elementclick.href);
 }
 
-
-/** xPath function */
-function xp(query) {
-    return document.evaluate(query, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
-}
+/** Adblock Warning banner */
+setTimeout(verifyLock, 500);
