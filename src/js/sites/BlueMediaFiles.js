bf4fc9b57157fdd3ef3d1f5738ac390cf5b21181
PushpenderSaini0 <54404738+PushpenderSaini0@users.noreply.github.com>
2021-06-21 17:50:43 +0200

    (#24) Rollback #xp() to a classic function at BlueMediaFiles.js

--- src/js/sites/BlueMediaFiles.js
+++ src/js/sites/BlueMediaFiles.js
@@ -20,6 +20,6 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 /** xPath function */
-const xp = (query) => {
+function xp(query) {
     return document.evaluate(query, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
